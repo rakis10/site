4acbd9654064b4608f6f08cb37c44f7f0c77e384
Juraj Rak <rakis10>
2020-09-14 18:50:01 +0200

    url works

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ var app = express()
 var http =  require('http')
 
 var server = http.Server(app)
+app.use(express.static(__dirname));
+
 
 express()
   .get('/', function(req, res)  {
     res.send('Hello World!')
   })
+app.get("/url", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+   });
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
   })
