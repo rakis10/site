2f3b46189ac4c4ef601e810e4ce7549917caa85a
rakis10 <jurajrak14@gmail.com>
2022-01-12 17:40:18 +0100

    asi nasaditelny fake api endpoint

--- vue-jan/server.js
+++ vue-jan/server.js
@@ -5,14 +5,7 @@ const serveStatic = require('serve-static')
 const path = require('path')
 
 
-
-
-
 const app = express()
-
-
-
-
 
 app.use('/',serveStatic(path.join(__dirname,'/dist')))
 
--- vue-jan/vue.config.js
+++ vue-jan/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy:{
             '^/api':{
-                target: ' http://localhost:5000',
+                target: ' https://still-dawn-01227.herokuapp.com/',
                 changeOrigin: true,
                 logLevel: 'debug',
                 pathRewrite: { '^/api': '/'}
