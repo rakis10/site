39fe8675fe692a5ec306846597c2912893375441
rakis10 <jurajrak14@gmail.com>
2022-04-06 13:35:31 +0200

    hotovy login

--- vue-jan/client/src/App.vue
+++ vue-jan/client/src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="container" >
-    <Hlavicka @showAddTask="toggleAddTask()" title="Rakis" :showAddTask="showAddTask" />
-<!--    Router vie poslat data do view , zachyti props-->
-    <router-view :showAddTask="showAddTask" ></router-view>
-    <Footer/>
+  <Navbar/>
+  <div class="container">
+  <router-view/>
   </div>
+  <!--  <div class="container" >-->
+<!--    <Hlavicka @showAddTask="toggleAddTask()" title="Rakis" :showAddTask="showAddTask" />-->
+<!--&lt;!&ndash;    Router vie poslat data do view , zachyti props&ndash;&gt;-->
+<!--    <router-view :showAddTask="showAddTask" ></router-view>-->
+<!--    <Footer/>-->
+<!--  </div>-->
 
 </template>
 <!--nezabudni na NPM RUN BUILD-->
@@ -13,11 +17,13 @@ import Hlavicka from "@/components/Hlavicka";
 import Tasks from "@/components/Tasks";
 import AddTask from "@/components/AddTask";
 import Footer from "@/components/Footer";
+import Navbar from "./components/Navbar";
 export default {
   name: 'App',
   components: {
     Footer,
     Hlavicka,
+    Navbar
   },
   data(){
     return{
@@ -34,45 +40,45 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-body {
-  font-family: 'Poppins', sans-serif;
-}
-.container {
-  max-width: 500px;
-  margin: 30px auto;
-  overflow: auto;
-  min-height: 300px;
-  border: 1px solid steelblue;
-  padding: 30px;
-  border-radius: 5px;
-}
-.btn {
-  display: inline-block;
-  background: #000;
-  color: #fff;
-  border: none;
-  padding: 10px 20px;
-  margin: 5px;
-  border-radius: 5px;
-  cursor: pointer;
-  text-decoration: none;
-  font-size: 15px;
-  font-family: inherit;
-}
-.btn:focus {
-  outline: none;
-}
-.btn:active {
-  transform: scale(0.98);
-}
-.btn-block {
-  display: block;
-  width: 100%;
-}
+/*@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');*/
+/** {*/
+/*  box-sizing: border-box;*/
+/*  margin: 0;*/
+/*  padding: 0;*/
+/*}*/
+/*body {*/
+/*  font-family: 'Poppins', sans-serif;*/
+/*}*/
+/*.container {*/
+/*  max-width: 500px;*/
+/*  margin: 30px auto;*/
+/*  overflow: auto;*/
+/*  min-height: 300px;*/
+/*  border: 1px solid steelblue;*/
+/*  padding: 30px;*/
+/*  border-radius: 5px;*/
+/*}*/
+/*.btn {*/
+/*  display: inline-block;*/
+/*  background: #000;*/
+/*  color: #fff;*/
+/*  border: none;*/
+/*  padding: 10px 20px;*/
+/*  margin: 5px;*/
+/*  border-radius: 5px;*/
+/*  cursor: pointer;*/
+/*  text-decoration: none;*/
+/*  font-size: 15px;*/
+/*  font-family: inherit;*/
+/*}*/
+/*.btn:focus {*/
+/*  outline: none;*/
+/*}*/
+/*.btn:active {*/
+/*  transform: scale(0.98);*/
+/*}*/
+/*.btn-block {*/
+/*  display: block;*/
+/*  width: 100%;*/
+/*}*/
 </style>
